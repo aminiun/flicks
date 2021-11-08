e85d002c8baba974c3a0caaf9c20d626b96e0730
amin <a.aminian321@gmail.com>
2021-11-08 20:00:54 +0330

    Image size fixed

--- apps/film/constants.py
+++ apps/film/constants.py
@@ -6,6 +6,6 @@ SEARCH_FILM_PAGE_SIZE = 10
 POSTER_WIDTH = 'w300'
 BANNER_WIDTH = 'w780'
 IMAGE_SIZE = '._V1_UX100_CR0,12,100,100_AL_.jpg'
-FILM_SIZE = '._V1_UY160_CR4,0,100,160_AL_.jpg'
+FILM_SIZE = '._V1_UY150_CR1,0,100,150_AL_.jpg'
 AMAZON_IMAGE_LINK = 'https://m.media-amazon.com/images/M/'
 TMDB_IMAGE_LINK = 'https://image.tmdb.org/t/p/'
