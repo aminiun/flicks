ba0065ed7892dcf3fd3780d55a4f79569ada1888
Amin <a.aminian321@gmail.com>
2023-02-11 00:08:16 +0330

    pytest added for tests

---
+++ flicks/settings/test.py
@@ -0,0 +1,32 @@
+import os
+
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+from .base import *         # noqa
+
+OTP_TEST_MODE = True
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+IMDB_API_KEY = "k_jijx5csi"
+
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
