6e3df8a8c85d47e49c2f8f7d1797a840a31ea658
Amin <a.aminian321@gmail.com>
2023-02-11 00:39:48 +0330

    old tests changed to pytest

--- apps/account/tests/test_views.py
+++ apps/account/tests/test_views.py
@@ -1,153 +1,157 @@
+import pytest
+
 from django.contrib.auth import get_user_model
 from django.core.cache import cache
 from django.urls import reverse
-from rest_framework.test import APITestCase
 
 from apps.account.models import PhoneOTP
 
 User = get_user_model()
 
 
-class TestAuth(APITestCase):
+@pytest.mark.django_db
+class TestAuth:
 
-    def tearDown(self) -> None:
+    @pytest.fixture(autouse=True)
+    def tear_down(self):
+        yield
         cache.clear()
 
-    def test_send_otp_for_not_existed_phone_success(self):
-        response = self.client.post(
+    def test_send_otp_for_not_existed_phone_success(self, client):
+        response = client.post(
             reverse('auth-send_otp'),
             data={
                 "phone": "+989122222111",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 201)
-        self.assertIsNotNone(response.json().get('id', None))
-        self.assertEqual(response.json()['phone'], "+989122222111")
+        assert response.status_code == 201
+        assert response.json().get('id', None)
+        assert response.json()['phone'], "+989122222111"
 
         user = PhoneOTP.active_objects.get(phone="+989122222111")
-        self.assertTrue(user.has_otp)
+        assert user.has_otp
 
-    def test_send_otp_for_existed_phone_before_time_limit_raise_validation_error_400(self):
+    def test_send_otp_for_existed_phone_before_time_limit_raise_validation_error_400(self, client):
         unverified_user = PhoneOTP.objects.create(
             phone='+989122222111',
         )
         unverified_user.set_otp()
-        response = self.client.post(
+        response = client.post(
             reverse('auth-send_otp'),
             data={
                 "phone": "+989122222111",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()['details'], "Try getting otp after 1 min")
+        assert response.status_code == 400
+        assert response.json()['details'] == "Try getting otp after 1 min"
 
-    def test_send_otp_for_existed_phone_in_phoneotp_model_success(self):
-        unverified_user = PhoneOTP.objects.create(
+    def test_send_otp_for_existed_phone_in_phoneotp_model_success(self, client):
+        PhoneOTP.objects.create(
             phone='+989122222111',
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-send_otp'),
             data={
                 "phone": "+989122222111",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 201)
-        self.assertIsNotNone(response.json().get('id', None))
-        self.assertEqual(response.json()['phone'], "+989122222111")
+        assert response.status_code == 201
+        assert response.json().get('id', None)
+        assert response.json()['phone'] == "+989122222111"
 
         user = PhoneOTP.active_objects.get(phone="+989122222111")
-        self.assertTrue(user.has_otp)
+        assert user.has_otp
 
-    def test_send_otp_for_existed_phone_in_user_model_raise_validation_error_400(self):
-        verified_user = User.objects.create(
+    def test_send_otp_for_existed_phone_in_user_model_raise_validation_error_400(self, client):
+        User.objects.create(
             username='test',
             phone='+989122222111'
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-send_otp'),
             data={
                 "phone": "+989122222111",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()['details'], ["User with that phone number already existed"])
+        assert response.status_code == 400
+        assert response.json()['details'] == "User with that phone number already existed"
 
-    def test_send_otp_for_wrong_phone_number_raise_validation_error_400(self):
-        response = self.client.post(
+    def test_send_otp_for_wrong_phone_number_raise_validation_error_400(self, client):
+        response = client.post(
             reverse('auth-send_otp'),
             data={
                 "phone": "09122222111",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code == 400
 
-    def test_verify_otp_for_existed_phoneotp_model_and_existed_otp_with_right_otp_success(self):
+    def test_verify_otp_for_existed_phoneotp_model_and_existed_otp_with_right_otp_success(self, client):
         unverified_user = PhoneOTP.objects.create(
             phone='+989122222111',
         )
-        otp = unverified_user.set_otp()
-        response = self.client.post(
+        unverified_user.set_otp()
+        response = client.post(
             reverse('auth-verify', kwargs={'pk': unverified_user.id}),
             data={
                 "otp": "1234",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(response.json().get('id', None))
+        assert response.status_code == 200
+        assert response.json().get('id', None)
 
-        self.assertTrue(unverified_user.is_verified)
+        assert unverified_user.is_verified
 
-    def test_verify_otp_for_not_existed_phoneotp_model_raise_404_not_found(self):
-        response = self.client.post(
+    def test_verify_otp_for_not_existed_phoneotp_model_raise_404_not_found(self, client):
+        response = client.post(
             reverse('auth-verify', kwargs={'pk': 1}),
             data={
                 "otp": "1234",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 404)
+        assert response.status_code == 404
 
-    def test_verify_otp_for_existed_phoneotp_model_and_existed_otp_with_wrong_otp_raise_400(self):
+    def test_verify_otp_for_existed_phoneotp_model_and_existed_otp_with_wrong_otp_raise_400(self, client):
         unverified_user = PhoneOTP.objects.create(
             phone='+989122222111',
         )
-        otp = unverified_user.set_otp()
-        response = self.client.post(
+        unverified_user.set_otp()
+        response = client.post(
             reverse('auth-verify', kwargs={'pk': unverified_user.id}),
             data={
                 "otp": "123456",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()['details'], 'OTP wrong!')
+        assert response.status_code == 400
+        assert response.json()['details'] == 'Entered code is wrong!'
 
-    def test_verify_otp_for_existed_phoneotp_model_and_not_existed_otp_raise_400(self):
+    def test_verify_otp_for_existed_phoneotp_model_and_not_existed_otp_raise_400(self, client):
         unverified_user = PhoneOTP.objects.create(
             phone='+989122222111',
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-verify', kwargs={'pk': unverified_user.id}),
             data={
                 "otp": "123456",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()['details'], 'OTP wrong!')
+        assert response.status_code == 400
+        assert response.json()['details'] == 'Your code is expired. Try to get it again'
 
-    def test_register_new_user_with_verified_phone_success(self):
+    def test_register_new_user_with_verified_phone_success(self, client):
         verified_user = PhoneOTP.objects.create(
             phone='+989122222111',
         )
         verified_user.verify()
-        response = self.client.post(
+        response = client.post(
             reverse('auth-register'),
             data={
                 "phone": "+989122222111",
@@ -156,16 +160,14 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 201)
-        self.assertTrue(
-            User.active_objects.active().filter(username="test", phone="+989122222111").exists()
-        )
+        assert response.status_code == 201
+        assert User.active_objects.active().filter(username="test", phone="+989122222111").exists()
 
-    def test_register_new_user_with_unverified_phone_raise_validation_error_400(self):
-        verified_user = PhoneOTP.objects.create(
+    def test_register_new_user_with_unverified_phone_raise_validation_error_400(self, client):
+        PhoneOTP.objects.create(
             phone='+989122222111',
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-register'),
             data={
                 "phone": "+989122222111",
@@ -174,15 +176,15 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()['details'], ['first verify phone number'])
+        assert response.status_code == 400
+        assert response.json()['details'] == ['first verify phone number']
 
-    def test_register_existed_user_raise_400(self):
-        existed_user = User.objects.create(
+    def test_register_existed_user_raise_400(self, client):
+        User.objects.create(
             phone='+989122222111',
             username='test',
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-register'),
             data={
                 "phone": "+989122222111",
@@ -191,14 +193,14 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code, 400
 
-    def test_register_new_user_with_existed_username_raise_validation_error_400(self):
-        existed_user = User.objects.create(
+    def test_register_new_user_with_existed_username_raise_validation_error_400(self, client):
+        User.objects.create(
             phone='+989122222111',
             username='test1',
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-register'),
             data={
                 "phone": "+989122222111",
@@ -207,14 +209,14 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code == 400
 
-    def test_register_new_user_with_existed_phone_raise_validation_error_400(self):
-        existed_user = User.objects.create(
+    def test_register_new_user_with_existed_phone_raise_validation_error_400(self, client):
+        User.objects.create(
             phone='+989122222112',
             username='test',
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-register'),
             data={
                 "phone": "+989122222111",
@@ -223,10 +225,10 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        assert response.status_code == 400
 
-    def test_login_with_not_existed_user_raise_404_error(self):
-        response = self.client.post(
+    def test_login_with_not_existed_user_raise_404_error(self, client):
+        response = client.post(
             reverse('auth-login'),
             data={
                 "phone_username": "+989191234567",
@@ -234,11 +236,11 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 404)
+        assert response.status_code == 404
 
-    def test_login_with_existed_user_with_invalid_password_raise_401_error(self):
-        user = User.objects.create_user(username='test', phone='+989191234567', password='pass')
-        response = self.client.post(
+    def test_login_with_existed_user_with_invalid_password_raise_401_error(self, client):
+        User.objects.create_user(username='test', phone='+989191234567', password='pass')
+        response = client.post(
             reverse('auth-login'),
             data={
                 "phone_username": "+989191234567",
@@ -246,12 +248,12 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 401)
-        self.assertEqual(response.json()['details'], 'Username or password is wrong!')
+        assert response.status_code == 401
+        assert response.json()['details'] == 'Username or password is wrong!'
 
-    def test_login_with_existed_user_with_phone_success(self):
-        user = User.objects.create_user(username='test', phone='+989191234567', password='pass')
-        response = self.client.post(
+    def test_login_with_existed_user_with_phone_success(self, client):
+        User.objects.create_user(username='test', phone='+989191234567', password='pass')
+        response = client.post(
             reverse('auth-login'),
             data={
                 "phone_username": "+989191234567",
@@ -259,15 +261,15 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
-    def test_login_with_existed_user_with_username_success(self):
-        user = User.objects.create_user(
+    def test_login_with_existed_user_with_username_success(self, client):
+        User.objects.create_user(
             username='test',
             phone='+989191234567',
             password='pass'
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-login'),
             data={
                 "phone_username": "test",
@@ -275,86 +277,86 @@ class TestAuth(APITestCase):
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
+        assert response.status_code == 200
 
-    def test_check_username_with_existed_username_success(self):
-        user = User.objects.create_user(
+    def test_check_username_with_existed_username_success(self, client):
+        User.objects.create_user(
             username='test',
             phone='+989191234567',
             password='pass'
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-check_username'),
             data={
                 "username": "test",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['details'], True)
+        assert response.status_code == 200
+        assert response.json()['details'] == True
 
-    def test_check_username_with_not_existed_username_success(self):
-        response = self.client.post(
+    def test_check_username_with_not_existed_username_success(self, client):
+        response = client.post(
             reverse('auth-check_username'),
             data={
                 "username": "test",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['details'], False)
+        assert response.status_code == 200
+        assert response.json()['details'] == False
 
-    def test_forget_password_otp_with_not_existed_user_raise_404(self):
-        response = self.client.post(
+    def test_forget_password_otp_with_not_existed_user_raise_404(self, client):
+        response = client.post(
             reverse('auth-forget_password_otp'),
             data={
                 "phone": "+989191234567",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 404)
+        assert response.status_code == 404
 
-    def test_forget_password_otp_with_existed_user_success(self):
+    def test_forget_password_otp_with_existed_user_success(self, client):
         otp = PhoneOTP.objects.create(phone='+989191234567')
         User.objects.create_user(
             username='test',
             phone='+989191234567',
             password='pass'
         )
-        response = self.client.post(
+        response = client.post(
             reverse('auth-forget_password_otp'),
             data={
                 "phone": "+989191234567",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertIsNotNone(response.json()['id'])
-        self.assertTrue(otp.has_otp)
+        assert response.status_code == 200
+        assert response.json()['id']
+        assert otp.has_otp
 
-    def test_forget_password_change_with_not_existed_phoneotp_raise_404(self):
-        response = self.client.post(
+    def test_forget_password_change_with_not_existed_phoneotp_raise_404(self, client):
+        response = client.post(
             reverse('auth-forget_password_change', kwargs={'pk': 1}),
             data={
                 "phone": "+989191234567",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 404)
+        assert response.status_code == 404
 
-    def test_forget_password_change_with_not_verified_phone_raise_400(self):
+    def test_forget_password_change_with_not_verified_phone_raise_400(self, client):
         otp = PhoneOTP.objects.create(phone='+989191234567')
-        response = self.client.post(
-            reverse('auth-forget_password_change', kwargs={'pk': 1}),
+        response = client.post(
+            reverse('auth-forget_password_change', kwargs={'pk': otp.id}),
             data={
                 "phone": "+989191234567",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json()['details'], 'first verify phone number')
+        assert response.status_code == 400
+        assert response.json()['details'] == 'First verify your phone number'
 
-    def test_forget_password_change_with_verified_phone_success(self):
+    def test_forget_password_change_with_verified_phone_success(self, client):
         otp = PhoneOTP.objects.create(phone='+989191234567')
         User.objects.create_user(
             username='test',
@@ -362,12 +364,12 @@ class TestAuth(APITestCase):
             password='pass'
         )
         otp.verify()
-        response = self.client.post(
+        response = client.post(
             reverse('auth-forget_password_change', kwargs={'pk': otp.id}),
             data={
                 "password": "new",
             },
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()['details'], "Password changed successfully")
+        assert response.status_code == 200
+        assert response.json()['details'] == "Password changed successfully"
